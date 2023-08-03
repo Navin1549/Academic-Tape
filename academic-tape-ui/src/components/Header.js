94d292dbebc8951b7eba0a3d29b79dbd168b35a4
navin-ranjan <navinranjan15498@gmail.com>
2023-08-03 08:30:18 +0530

    add component react

---
+++ academic-tape-ui/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return(
+        <div>
+            <li>header</li>
+            <ol>home</ol>
+        </div>
+    );
+    
+}
+export default Header;
